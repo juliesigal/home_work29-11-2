e8ca5786bbcdfe73591c4dfa575d3536d0c99642
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-30 18:12:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,3 +35,16 @@ class Worker(Employee):
     def __str__(self):
         return super().__str__() + f"worker's hours per day is: {self.hours_per_day} " +
                f"worker's rate per hours is: {self.hour_rate}"
+
+   class Kablan(Employee):
+    def __init__(self, id, name, address, age, num_project, project_rate):
+        super().__init__()
+        self.num_project = num_project
+        self.project_rate = project_rate
+
+    def calculateSalary(self, num_project, project_rate):
+        return self.num_project * self.project_rate
+
+    def __str__(self):
+        return super().__str__() + f"kablan's numbers of projecs is: {self.num_project} " +
+        f"kablan's rate per project is: {self.project_rate}"     
